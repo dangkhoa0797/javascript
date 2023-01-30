388fb2f1727057e9543403c6c836325dd6be7a99
khoahd <hoangdangkhoa0797@gmail.com>
2023-01-30 16:27:49 +0700

    k

--- 1/main.js
+++ 1/main.js
@@ -1,1 +1,15 @@
-alert('Hi javascript basic')
+//alert('Hi javascript basic')
+
+// var fullName='hoang dang khoa'
+// var age=26
+// 
+// alert(fullName)
+// alert(age)
+
+//console.log('khoa')
+
+var a=1
+var b=2
+if (a<b) {
+    alert('khoa')
+}
